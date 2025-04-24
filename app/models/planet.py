01131645284c8fe708feff192cbf863decdc058e
Dana Al Shaar <da.alshaar@gmail.com>
2025-04-24 16:22:53 -0700

    wave 3 passed

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,16 +1,25 @@
-class Planet:
-    def __init__(self, id, name, description):
-        self.id = id
-        self.name = name
-        self.description = description
+# class Planet:
+#     def __init__(self, id, name, description):
+#         self.id = id
+#         self.name = name
+#         self.description = description
 
-planets = [
-    Planet(1, "Mercury", "The closest planet to the Sun, the smallest with heavily cratered surface."), 
-    Planet(2, "Venus", "The hottest planet in our solar system, with a thick, toxic atmosphere"),
-    Planet(3, "Earth", "The third planet from the Sun, and the only known planet to support life."),
-    Planet(4, "Mars", "Known as the Red Planet due to its iron-rich soil, it has polar ice caps and canyons."),
-    Planet(5, "Jupiter", "The largest planet in our solar system, with a prominent Great Red Spot (a giant storm)."),
-    Planet(6, "Saturn", "Famous for its prominent ring system, composed of ice and rock particles."),
-    Planet(7, "Uranus", "A gas giant with a unique tilt, often called the icy giant."),
-    Planet(8, "Neptune", "The furthest planet from the Sun, also considered an icy giant.")
-]
+# planets = [
+#     Planet(1, "Mercury", "The closest planet to the Sun, the smallest with heavily cratered surface."), 
+#     Planet(2, "Venus", "The hottest planet in our solar system, with a thick, toxic atmosphere"),
+#     Planet(3, "Earth", "The third planet from the Sun, and the only known planet to support life."),
+#     Planet(4, "Mars", "Known as the Red Planet due to its iron-rich soil, it has polar ice caps and canyons."),
+#     Planet(5, "Jupiter", "The largest planet in our solar system, with a prominent Great Red Spot (a giant storm)."),
+#     Planet(6, "Saturn", "Famous for its prominent ring system, composed of ice and rock particles."),
+#     Planet(7, "Uranus", "A gas giant with a unique tilt, often called the icy giant."),
+#     Planet(8, "Neptune", "The furthest planet from the Sun, also considered an icy giant.")
+# ]
+
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    
--- app/routes/planet_route.py
+++ app/routes/planet_route.py
@@ -1,41 +1,80 @@
-from app.models.planet import planets
-from flask import Blueprint, abort, make_response 
+from app.models.planet import Planet
+from ..db import db
+from flask import Blueprint, abort, make_response, request
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    
+    new_planet = Planet(name=name, description=description)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+    "id": new_planet.id,
+    "name": new_planet.name,
+    "description": new_planet.description
+}
+
+
+    
+
+    return response, 201
+
 @planets_bp.get("")
 def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
     planets_response = []
     for planet in planets:
         planets_response.append(
             {
                 "id": planet.id,
                 "name": planet.name,
-                "description": planet.description
+                "description": planet.description  
             }
-        )
+    )
+
+
     return planets_response
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planets_bp.get("")
+# def get_all_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description
+#             }
+#         )
+#     return planets_response
 
-    return {
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description
-    }
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+
+#     return {
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description
+#     }
 
     
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        response = {"message": f"planet {planet_id} invalid"}
-        abort(make_response(response, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         response = {"message": f"planet {planet_id} invalid"}
+#         abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
 
-    response = {"message": f"planet {planet_id} not found"}
-    abort(make_response(response, 404))
+#     response = {"message": f"planet {planet_id} not found"}
+#     abort(make_response(response, 404)
